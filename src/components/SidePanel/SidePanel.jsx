eec26a34481e63153eb1b5fb1cf0bcc79f66c1f3
Nikita Maxakov (lab64PC) <iamlegkiy@gmail.com>
2022-10-26 17:32:28 +0800

    playing with radius

--- src/components/SidePanel/SidePanel.jsx
+++ src/components/SidePanel/SidePanel.jsx
@@ -1,6 +1,16 @@
 import './SidePanel.scss';
+import isemLogo from './isem-logo.png';
 
 const SidePanel = () => {
-  return <div className="sidePanel"></div>;
+  return (
+    <div className="sidePanel">
+      <div className="side-el">
+        <a className="isemLogo" href="https://isem.irk.ru/">
+          <img src={isemLogo} alt="logo"></img>
+          ИСЭМ СО РАН
+        </a>
+      </div>
+    </div>
+  );
 };
 export default SidePanel;
