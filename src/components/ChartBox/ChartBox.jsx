cade50ef009941203b81d869aef4858dbd1712ab
Nikita Maxakov (lab64PC) <iamlegkiy@gmail.com>
2022-10-28 16:26:14 +0800

    need to merge obj from csv to arr

--- src/components/ChartBox/ChartBox.jsx
+++ src/components/ChartBox/ChartBox.jsx
@@ -1,7 +1,35 @@
 import './ChartBox.scss';
+import * as d3 from 'd3';
 import csvFile from './data/2022-09.csv';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Bar } from 'react-chartjs-2';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
 const ChartBox = () => {
+  const solarData = d3.csv(csvFile, function (data) {
+    let newData;
+    for (let key in data) {
+      newData += data;
+    }
+    console.log(newData);
+  });
+
   return <div className="chartBox"></div>;
 };
 export default ChartBox;
