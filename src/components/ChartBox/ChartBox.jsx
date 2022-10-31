6c5926beef618895bf6c81dc83e5ec6a57cb63c0
Nikita Maxakov (lab64PC) <iamlegkiy@gmail.com>
2022-10-31 12:17:47 +0800

    create map on data, need to deal with the delay in parsing

--- src/components/ChartBox/ChartBox.jsx
+++ src/components/ChartBox/ChartBox.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import './ChartBox.scss';
 import * as d3 from 'd3';
 import csvFile from './data/2022-09.csv';
@@ -5,17 +6,19 @@ import {
   Chart as ChartJS,
   CategoryScale,
   LinearScale,
-  BarElement,
+  PointElement,
+  LineElement,
   Title,
   Tooltip,
   Legend,
 } from 'chart.js';
-import { Bar } from 'react-chartjs-2';
+import { Line } from 'react-chartjs-2';
 
 ChartJS.register(
   CategoryScale,
   LinearScale,
-  BarElement,
+  PointElement,
+  LineElement,
   Title,
   Tooltip,
   Legend
@@ -24,17 +27,43 @@ ChartJS.defaults.font.family = 'PT Sans';
 ChartJS.defaults.font.size = 14;
 ChartJS.defaults.color = 'black';
 
-const ChartBox = () => {
-  const arr = [];
-  d3.csv(csvFile, function (file) {
-    arr.push(file);
-  });
-  console.log(arr);
+const arr = [];
+//date time amp1 amp2 amp3 v1 v2 v3
+d3.csv(csvFile, function (file) {
+  arr.push(file);
+});
 
-  const data = {};
+const ChartBox = () => {
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setShow(true);
+    }, 300);
+    return () => clearTimeout(timeout);
+  }, [show]);
+
+  const day = arr.map((file) => file.date);
+  const amp = arr.map((file) => file.amp2);
+  console.log(arr);
+  console.log(day);
+  const data = {
+    labels: day,
+    datasets: [
+      {
+        label: 'amp',
+        data: amp,
+        borderColor: 'rgb(255, 99, 132)',
+      },
+    ],
+  };
 
   const options = {};
 
-  return <div className="chartBox"></div>;
+  return (
+    <div className="chartBox">
+      <Line data={data} options={options} />
+    </div>
+  );
 };
 export default ChartBox;
