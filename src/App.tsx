494ff566cb95ced402f319e4eb4c1918b0c2cbdd
Nikita Maxakov (lab64PC) <iamlegkiy@gmail.com>
2022-11-11 10:50:48 +0800

    move all app no typeScript

---
+++ src/App.tsx
@@ -0,0 +1,54 @@
+import { useState } from 'react';
+import './App.css';
+import SidePanel from './components/SidePanel/SidePanel';
+import MainBox from './components/MainBox/MainBox';
+import csvFile from './components/ChartBox/data/2022-09.csv';
+
+import * as d3 from 'd3';
+
+function App() {
+  const [yAxis1, setYAxis] = useState();
+
+  // let csv = '../../../../Downloads/public/2022-09.csv';
+  let csv: string = '/2022-09.csv';
+
+  let url: string = `https://files.isem.irk.ru/remote.php/dav/files/nikita.max/%D0%94%D0%B0%D0%BD%D0%BD%D1%8B%D0%B5-%D1%81%D1%82%D0%B5%D0%BD%D0%B4%D0%B0/2022-09.csv`;
+
+  let username: 'nikita.max' = 'nikita.max';
+  let password: 'E4DqvJacNxSW' = 'E4DqvJacNxSW';
+
+  let headers = new Headers();
+  headers.set('Authorization', 'Basic ' + btoa(username + ':' + password));
+
+  async function getData() {
+    const res = await fetch(url, {
+      headers,
+    });
+    const data = await res.text();
+    console.log(data);
+    const table = data.split('\n');
+    const tableLenght = table.length - 1;
+    const newTable = table.splice(0, tableLenght);
+    newTable.forEach((row) => {
+      const columns = row.split(',');
+      const date = columns[0];
+    });
+  }
+
+  getData();
+
+  const arr: [] = [];
+
+  const promise = d3.csv(csvFile, function (file:any) {
+    arr.push(file);
+  });
+
+  return (
+    <div className="App">
+      <SidePanel arr={arr} promise={promise} />
+      <MainBox arr={arr} promise={promise} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/SidePanel/SidePanel.tsx
@@ -0,0 +1,96 @@
+import './SidePanel.scss';
+import isemLogo from './isem-logo.png';
+import SideElement from './SideElement/SideElement';
+import Select from '../Select/Select';
+import { useEffect, useState } from 'react';
+
+const SidePanel = ({ arr, promise }) => {
+  const [show, setShow] = useState(false);
+  const [chosenDayStart, setChosenDayStart] = useState('start');
+  const [chosenDayEnd, setChosenDayEnd] = useState('end');
+
+  const [selectedMonth, setSelectedMonth] = useState('...');
+
+  const days = arr.map((file) => file.date);
+  const day = days.filter((item, index) => {
+    return days.indexOf(item) === index;
+  });
+
+  //get yaer + month for file selection
+  let fileOptions = [];
+  let fileValue = [];
+  //цикл для перебора годов
+  for (let year = 2022; year < 2073; year++) {
+    //цикл для перебора месяцев внутри каждого года
+    for (let month = 1; month < 13; month++) {
+      //условие минимального значения
+      if (year === 2022 && month < 8) month = 8;
+      //условие для формата даты месяца меньше 10
+      if (month < 10) month = '0' + month;
+
+      const monthYear = month + '.' + year;
+      const monthYearFile = year + '-' + month;
+      fileOptions.push(monthYear);
+      fileValue.push(monthYearFile);
+    }
+  }
+
+  useEffect(() => {
+    promise.then(() => {
+      setShow(true);
+    });
+  }, [show, promise]);
+
+  return (
+    <div className="sidePanel">
+      <SideElement
+        elementInside={
+          <a className="isemLogo" href="https://isem.irk.ru/">
+            <img src={isemLogo} alt="logo"></img>
+            ИСЭМ СО РАН
+          </a>
+        }
+      />
+      <SideElement
+        elementInside={
+          <Select
+            selected={selectedMonth}
+            setSelected={setSelectedMonth}
+            options={fileOptions}
+            value={fileValue}
+            noSelect="..."
+          />
+        }
+      />
+      <SideElement
+        elementInside={
+          <>
+            <Select
+              selected={chosenDayStart}
+              setSelected={setChosenDayStart}
+              options={day}
+              noSelect="start"
+            />
+            -
+            <Select
+              selected={chosenDayEnd}
+              setSelected={setChosenDayEnd}
+              options={day}
+              noSelect="end"
+            />
+          </>
+        }
+      />
+      <SideElement
+        elementInside={
+          <p>
+            Selected day
+            <br />
+            {chosenDayStart} - {chosenDayEnd}
+          </p>
+        }
+      />
+    </div>
+  );
+};
+export default SidePanel;
