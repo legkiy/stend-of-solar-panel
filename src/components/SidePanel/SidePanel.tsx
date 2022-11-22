bd92e735cd1eba406e0bb1fbacf478674a8c2ae6
Nikita Maxakov (lab64PC) <iamlegkiy@gmail.com>
2022-11-22 12:37:19 +0800

    added redux

--- src/components/SidePanel/SidePanel.tsx
+++ src/components/SidePanel/SidePanel.tsx
@@ -1,14 +1,37 @@
 import Select from './Select';
 import './SidePanel.scss';
+import { useDispatch, useSelector } from 'react-redux';
+import { setPanelNumber } from '../../features/panel/panelSlice';
+import { RootState } from '../../store/store';
 
 interface IProprs {
   setPanel: React.Dispatch<React.SetStateAction<number>>;
 }
 
 const SidePanel = ({ setPanel }: IProprs) => {
+  const dispatch = useDispatch();
+  const panel = useSelector((state: RootState) => state.panel.panelNumber);
+
   return (
     <div className="sidePanel">
       <Select setPanel={setPanel} />
+      <div style={{ margin: '20px', background: 'black' }}>
+        <input
+          onChange={(e) => {
+            dispatch(setPanelNumber(e.target.value));
+          }}
+        />
+        <select
+          className="select"
+          onChange={(e) => {
+            dispatch(setPanelNumber(e.target.value));
+          }}
+        >
+          <option value={0}>0</option>
+          <option value={1}>1</option>
+        </select>
+      </div>
+      <div>{panel}</div>
     </div>
   );
 };
